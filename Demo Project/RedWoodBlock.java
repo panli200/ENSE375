19d16d08d4e3fb6e8906673da257225b101005fb
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-02-17 13:26:31 -0600

    Missing @Override tag

--- Demo Project/RedWoodBlock.java
+++ Demo Project/RedWoodBlock.java
@@ -4,6 +4,7 @@ public class RedWoodBlock.java
 		display() is overridden in the RedMetalBlock class to reflect the material changing from Plastic to Wood.
 	*/
 	
+	@Override
 	public void display()
 	{
 		System.out.println("Red Wood Block created");
