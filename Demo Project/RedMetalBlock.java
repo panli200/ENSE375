247b39e85db181f5537f26a894de459fd0e4f97c
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-02-17 13:26:12 -0600

    Missing @Override tag

--- Demo Project/RedMetalBlock.java
+++ Demo Project/RedMetalBlock.java
@@ -4,6 +4,7 @@ public class RedMetalBlock.java
 		display() is overridden in the RedMetalBlock class to reflect the material changing from Plastic to Metal.
 	*/
 	
+	@Override
 	public void display()
 	{
 		System.out.println("Red Metal Block created");
