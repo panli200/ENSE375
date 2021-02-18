71e0a8b9b235c004ae10020457ec4e192f2b045d
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-02-17 18:25:51 -0600

    Create BlueWoodBlock.java

---
+++ Demo Project/BlueWoodBlock.java
@@ -0,0 +1,12 @@
+public class BlueWoodBlock extends BlueBlock
+{
+	/*
+		display() is overridden in the BlueMetalBlock class to reflect the material changing from Plastic to Wood.
+	*/
+	
+	@Override
+	public void display()
+	{
+		System.out.println("Blue Wood Block created");
+	}
+}
