8ea6323fa1ddc15fe3ca2a861cfe18b5ab1a83d2
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-02-17 18:20:47 -0600

    Create BlueBlock.java

---
+++ Demo Project/BlueBlock.java
@@ -0,0 +1,12 @@
+public class BlueBlock extends Block
+{
+    /*
+        The display() method is overridden to account for the Block's colour changing to blue.
+    */
+    
+    @Override
+    public void display()
+    {
+        System.out.println("Blue Plastic Block created");
+    }
+}
