7c045a5ecf7cecffe96fe3469cf6b73e3c4b8c8c
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-02-17 18:35:55 -0600

    Update BlockProgram.java

--- Demo Project/BlockProgram.java
+++ Demo Project/BlockProgram.java
@@ -7,12 +7,12 @@ public class BlockProgram
         This file is heavily derived from the Winter 2021 ENSE 370 Lab #3 Assignment, which was written by Adam Tilson. It can be found at https://github.com/adamtilson/ense370/blob/main/lab-3/lab-doc.md
     */
   
-    public BlockFactory blockFactory;
+    public BlockFactory blockFactory();
     public ArrayList<Block> blocks;
   
     public BlockProgram()
     {
-        blockFactory = new BlockFactory();
+        blockFactory = new BlueBlockFactory();
         blocks = new ArrayList<Block>();
     }
   
@@ -27,6 +27,15 @@ public class BlockProgram
         blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Wood"));
         blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Wood"));
       
+        blockProgram.blockFactory = new RedBlockFactory();
+        
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Plastic"));
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Plastic"));
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Metal"));
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Metal"));
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Wood"));
+        blockProgram.blocks.add(blockProgram.blockFactory.createBlock("Wood"));
+        
         for (Block block : blockProgram.blocks)
             block.display();
       
