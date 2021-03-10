7d5eb4b76eda748ce3f368ba815bc25395c5143c
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-10 15:38:57 -0600

    Add files via upload

---
+++ PatientList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class PatientList
+{
+    private ArrayList<Patient> patientList; // list of Patients
+
+    public PatientList()
+    {
+	patientList = new ArrayList<Patient>();
+    }
+
+    /**
+     *
+     * @return boolean: true if the patient can be added to the list
+     */
+    public boolean addPatient(Patient patient)
+    {
+        patientList.add(patient);
+	return true;
+    }
+
+    public boolean deletePatient(int index)
+    {
+	patientList.delete(index);
+	return true;
+    }
+
+    public Patient getPatient(String id)
+    {
+	for (Patient thisPatient : patientList)
+	    {
+		if (thisPatient.getID().equals(id))
+		    return thisPatient;
+	    }
+	
+	return null;
+    }
+
+    public int getNumberofPatients()
+    {
+	return patients.size();
+}
