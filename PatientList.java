09c4770dc61cff7e86d18813bc071ab28a94d781
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-10 15:40:15 -0600

    Update PatientList.java

--- PatientList.java
+++ PatientList.java
@@ -18,13 +18,21 @@ public class PatientList
         patientList.add(patient);
 	return true;
     }
-
+	
+    /**
+     *
+     * @return boolean: true if the patient is deleted from the list
+     */
     public boolean deletePatient(int index)
     {
 	patientList.delete(index);
 	return true;
     }
 
+    /**
+     *
+     * @return Patient: non-null if the patient with the particular ID is in the list
+     */
     public Patient getPatient(String id)
     {
 	for (Patient thisPatient : patientList)
@@ -36,6 +44,10 @@ public class PatientList
 	return null;
     }
 
+    /**
+     *
+     * @return int: returns the current size of the list
+     */	
     public int getNumberofPatients()
     {
 	return patients.size();
