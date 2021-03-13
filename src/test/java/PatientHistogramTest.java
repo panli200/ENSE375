02d0c2a48ea74f77d71e8f1eafd41a1c27cdda4f
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-13 17:09:05 -0600

    Update PatientHistogramTest.java

--- src/test/java/PatientHistogramTest.java
+++ src/test/java/PatientHistogramTest.java
@@ -1,19 +1,23 @@
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.*;
+import static org.junit.Assert.AsserEquals;
+import org.junit.Test;
 
 /**
  * Unit test for PatientHistogram.
  */
 public class PatientHistogramTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+	@Test
+    public void Add_Histo() {
+		PatientHistogram patientHistogram = new PatientHistogram();
+		patientHistogram.addAPatientToRegion(1, 4);
+        assertEquals(1, patientHistogram.getPatientsCountInRegion(1, 4)); //expected, actual
+    }
+	
+	
+	@Test
+    public void Delete_Histo() {
+		PatientHistogram patientHistogram = new PatientHistogram();
+		patientHistogram.deleteAPatientFromRegion(1, 4);
+        assertEquals(0, patientHistogram.getPatientsCountInRegion(1, 4)); //expected, actual
     }
 }
