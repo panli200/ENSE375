42b0cb3717c4abbf1e0b71b80b588be3073b425e
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-13 15:55:50 -0600

    Added missing parentheses

--- src/main/java/PatientList.java
+++ src/main/java/PatientList.java
@@ -28,7 +28,7 @@ public class PatientList
 	int initialSize = this.getNumberofPatients();
 	patientList.remove(index);
 	
-	if (this.getNumberofPatients < initialSize) // indicating that an element has been removed
+	if (this.getNumberofPatients() < initialSize) // indicating that an element has been removed
 	    return true;
 	    
 	else return false; // the sizes are the same, indicating that no element was removed
