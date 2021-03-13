8ed0e57962737971db92e9990f7f89646d6771e7
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-13 17:08:27 -0600

    Update RiskCodeMapTest.java

--- src/test/java/RiskCodeMapTest.java
+++ src/test/java/RiskCodeMapTest.java
@@ -1,19 +1,71 @@
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import static org.junit.Assert.AsserEquals;
 
-import java.io.*;
+public class RiskCodeMapTest {
 
-/**
- * Unit test for RiskCodeMap.
- */
-public class RiskCodeMapTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+	
+	@Test
+    public void TestG() {
+		RiskCodeMap riskCodeMap = new RiskCodeMap();
+		ArrayList<Integer> neighboursCaseCount = new ArrayList<Integer>();
+
+		neighboursCaseCount.add(0);
+		neighboursCaseCount.add(0);
+		neighboursCaseCount.add(0);
+		riskCodeMap.updateRiskInARegion(1, 1, 0, neighboursCaseCount);
+        assertEquals('G',riskCodeMap.getRiskInARegion(1, 1)); //expected, actual
     }
+	
+	
+	@Test
+    public void TestB() {
+		RiskCodeMap riskCodeMap = new RiskCodeMap();
+		ArrayList<Integer> neighboursCaseCount = new ArrayList<Integer>();
+
+		neighboursCaseCount.add(2);
+		neighboursCaseCount.add(2);
+		neighboursCaseCount.add(2);
+		riskCodeMap.updateRiskInARegion(1, 1, 2, neighboursCaseCount);
+        assertEquals('B',riskCodeMap.getRiskInARegion(1, 1)); //expected, actual
+    }
+	
+	@Test
+    public void TestY() {
+		RiskCodeMap riskCodeMap = new RiskCodeMap();
+		ArrayList<Integer> neighboursCaseCount = new ArrayList<Integer>();
+
+		neighboursCaseCount.add(5);
+		neighboursCaseCount.add(5);
+		neighboursCaseCount.add(5);
+		riskCodeMap.updateRiskInARegion(1, 1, 5, neighboursCaseCount);
+        assertEquals('Y',riskCodeMap.getRiskInARegion(1, 1)); //expected, actual
+    }
+	
+	
+	@Test
+    public void TestO() {
+		RiskCodeMap riskCodeMap = new RiskCodeMap();
+		ArrayList<Integer> neighboursCaseCount = new ArrayList<Integer>();
+
+		neighboursCaseCount.add(9);
+		neighboursCaseCount.add(9);
+		neighboursCaseCount.add(9);
+		riskCodeMap.updateRiskInARegion(1, 1, 9, neighboursCaseCount);
+        assertEquals('O',riskCodeMap.getRiskInARegion(1, 1)); //expected, actual
+    }
+	
+	@Test
+    public void TestR() {
+		RiskCodeMap riskCodeMap = new RiskCodeMap();
+		ArrayList<Integer> neighboursCaseCount = new ArrayList<Integer>();
+
+		neighboursCaseCount.add(15);
+		neighboursCaseCount.add(15);
+		neighboursCaseCount.add(15);
+		riskCodeMap.updateRiskInARegion(1, 1, 15, neighboursCaseCount);
+        assertEquals('R',riskCodeMap.getRiskInARegion(1, 1)); //expected, actual
+    }
+	
+	
+
 }
