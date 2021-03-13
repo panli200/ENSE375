8676b1f188be7fd6fa57fb7374f284643bf84808
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-03-14 02:19:02 +0300

    Update PatientHistogramTest.java

--- src/test/java/PatientHistogramTest.java
+++ src/test/java/PatientHistogramTest.java
@@ -1,5 +1,5 @@
-import static org.junit.Assert.AsserEquals;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for PatientHistogram.
