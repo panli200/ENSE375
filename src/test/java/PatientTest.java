10fd1ee2582bbb5863614582d85bcfcbb8e394d5
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-03-14 12:20:43 -0600

    Update PatientTest.java

--- src/test/java/PatientTest.java
+++ src/test/java/PatientTest.java
@@ -18,9 +18,9 @@ public class PatientTest {
     }
 
 	// Exception thrown due to name including numbers and characters. 
-    @Test(expected = InvalidNameException.class)
+    @Test
     public void invalidPatientName() throws Exception {
-        Patient patient = new Patient("Alice5!", "200390111", 21, new PostalCode("K1E-1A4"));
+        assertThrows(InvalidNameException.class, () -> { Patient patient = new Patient("Alice5!", "200390111", 21, new PostalCode("K1E-1A4");});
     }
 
 	// Exception thrown due to negative age value.
