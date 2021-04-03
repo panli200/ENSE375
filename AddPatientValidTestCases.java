a9132733cc7f84f3bd21b92a10ce0b818a82b060
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-04-03 22:11:23 +0300

    Update AddPatientValidTestCases.java

--- AddPatientValidTestCases.java
+++ AddPatientValidTestCases.java
@@ -1,10 +1,21 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.*;
 
 public class AppPatientValidTestCases()
 {
+	
+	
+	@Test				
+	public void testSelenium() {	
+	driver.get("http://demo.guru99.com/test/guru99home/");  
+	String title = driver.getTitle();				 
+	Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}
+	
 	@Test
 	public void NormaAdd(){
 	App app = new App();
