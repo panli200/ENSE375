cedbec91bf72d6bb8530b36c6c4dd54c7a088873
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-04-03 21:35:08 +0300

    Update AddPatientValidTestCases.java

--- AddPatientValidTestCases.java
+++ AddPatientValidTestCases.java
@@ -6,47 +6,27 @@ import java.io.*;
 public class AppPatientValidTestCases()
 {
 	@Test
-	public void NormalDelete(){
+	public void NormaAdd(){
 	App app = new App();
-	app.addPatient("Patient1", "1", "S4S-4S4", 10);
-	boolean DeleteNormal = app.deletePatient("1");
-	assertEquals(true, DeleteFirst);
+	boolean AddNormal = app.addPatient("Patient1", "1", "S4S-4S4", 10);
+	assertEquals(true, AddNormal);
 	}
 
 	@Test
-	public void NormalDeleteBoundsFirst(){
+	public void NormalAddMoreThanOne(){
 	App app = new App();
-	app.addPatient("Patient1", "1", "S4S-4S4", 10);
-	app.addPatient("Patient2", "2", "S4S-4S5", 10);
-	app.addPatient("Patient3", "3", "S4S-4S6", 10);
-	boolean DeleteFirst = app.deletePatient("1");
-	assertEquals(true, DeleteFirst);
+	boolean AddFirst =  app.addPatient("Patient1", "1", "S4S-4S4", 10);
+	boolean AddLast = app.addPatient("Patient3", "3", "S4S-4S6", 10);
+	assertEquals(true, AddFirst);
+	assertEquals(true, AddLast);
 	}
-
 	@Test
-	public void NormalDeleteBoundsLast(){
+	public void NormalAddAge0(){
 	App app = new App();
-	app.addPatient("Patient1", "1", "S4S-4S4", 10);
-	app.addPatient("Patient2", "2", "S4S-4S5", 10);
-	app.addPatient("Patient3", "3", "S4S-4S6", 10);
-	boolean DeleteLast = app.deletePatient("3");
-	assertEquals(true, DeleteFirst);
+	boolean AddFirst =  app.addPatient("Patient1", "1", "S4S-4S4", 0);
+	boolean AddLast = app.addPatient("Patient3", "3", "S4S-4S6", 0);
+	assertEquals(true, AddFirst);
+	assertEquals(true, AddLast);
 	}
-
-	@Test
-	public void NormalDeleteMultiple(){
-	App app = new App();
-	app.addPatient("Patient1", "1", "S4S-4S4", 10);
-	app.addPatient("Patient2", "2", "S4S-4S5", 10);
-	app.addPatient("Patient3", "3", "S4S-4S6", 10);
-
-	boolean DeleteMultiple1 = app.deletePatient("3");
-	boolean DeleteMultiple2 = app.deletePatient("2");
-	boolean DeleteMultiple3 = app.deletePatient("1");
-	assertEquals(true, DeleteMultiple1);
-	assertEquals(true, DeleteMultiple2);
-	assertEquals(true, DeleteMultiple3);
-	}
-	
 
 }
