2907cfbb28eba3cfba0428d227fcca23ae624f61
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-03 13:39:43 -0600

    Update AddPatientValidTest.java

--- src/test/java/AddPatientValidTest.java
+++ src/test/java/AddPatientValidTest.java
@@ -11,7 +11,7 @@ public class AddPatientValidTest
 	
 	@Test				
 	public void testSelenium() {	
-	private WebDriver driver;
+	WebDriver driver = new WebDriver();
 	driver.get("http://demo.guru99.com/test/guru99home/");  
 	String title = driver.getTitle();				 
 	assertTrue(title.contains("Demo Guru99 Page")); 		
