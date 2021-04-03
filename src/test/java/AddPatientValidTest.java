77b54fb0be36844784c65624aeec49bfc3901cdd
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-04-04 00:42:27 +0300

    Update AddPatientValidTest.java

--- src/test/java/AddPatientValidTest.java
+++ src/test/java/AddPatientValidTest.java
@@ -11,19 +11,18 @@ public class AddPatientValidTest
 {
 	
 	
-	//@Test				
-	//public void testSelenium() {
-	
-	//WebDriver driver = new FirefoxDriver();
-	//driver.get("http://demo.guru99.com/test/guru99home/");  
-	//String title = driver.getTitle();
-	//driver.quit();	
-	//assertTrue(title.contains("Demo Guru99 Page")); 		
-	//}
+	@Test				
+	public void testSelenium() {
+	System.setProperty("webdriver.gecko.driver", "Users/rabaa/Downloads/geckodriver");	
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://demo.guru99.com/test/guru99home/");  
+	String title = driver.getTitle();
+	driver.quit();	
+	assertTrue(title.contains("Demo Guru99 Page")); 		
+	}
 	
 	@Test
 	public void NormaAdd(){
-	System.setProperty("webdriver.gecko.driver", "Users/rabaa/Downloads/geckodriver");
 	App app = new App();
 	boolean AddNormal = app.addPatient("Patient One", "200000001", "K1S-4S4", 10);
 	assertEquals(true, AddNormal);
