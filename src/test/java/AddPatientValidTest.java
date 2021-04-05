1dfd51ba86a273b4c132b7f3b73cfeca2bb5a054
Rabaa-basha <55857464+Rabaa-basha@users.noreply.github.com>
2021-04-06 00:02:17 +0300

    Update AddPatientValidTest.java

--- src/test/java/AddPatientValidTest.java
+++ src/test/java/AddPatientValidTest.java
@@ -19,17 +19,17 @@ public class AddPatientValidTest
 	@Test				
 	public void testSelenium() throws Exception{
 
-//	System.setProperty("webdriver.chrome.driver", "/Applications/GoogleChrome.app/Contents/MacOS/GoogleChrome");
-// 	WebDriver driver = new ChromeDriver();
+	System.setProperty("webdriver.chrome.driver", "/Applications/GoogleChrome.app/Contents/MacOS/GoogleChrome");
+	WebDriver driver = new ChromeDriver();
 // 	DesiredCapabilities cap = DesiredCapabilities.firefox();
 // 	cap.setCapability("marionette", true);
 // 	WebDriver driver = new FirefoxDriver(cap);
-	File gecko = new File ("geckodriver");
-	gecko.setExecutable(true);
-	System.setProperty("webdriver.firefox.bin", "/home/rabaa/geckodriver");
- 	System.setProperty("webdriver.gecko.driver", "/home/rabaa/geckodriver⁩");
-	WebDriver driver = new FirefoxDriver();
-	driver.get("http://demo.guru99.com/test/guru99home/");  
+// 	File gecko = new File ("geckodriver");
+// 	gecko.setExecutable(true);
+// 	System.setProperty("webdriver.firefox.bin", "/home/rabaa/geckodriver");
+//  	System.setProperty("webdriver.gecko.driver", "/home/rabaa/geckodriver⁩");
+// 	WebDriver driver = new FirefoxDriver();
+// 	driver.get("http://demo.guru99.com/test/guru99home/");  
 	String title = driver.getTitle();		
 	}
 	
